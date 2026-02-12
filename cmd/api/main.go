5f22896711d2fc30c1966d89062bd1f4aa489fac
Sagiri721 <1231108@isep.ipp.pt>
2026-02-12 20:28:32 +0000

    fix: add set database path from go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,21 @@
 package main
 
+/*
+#cgo linux LDFLAGS: -L${SRCDIR}/../../drivers -Wl,-rpath,${SRCDIR}/../../../drivers -lanime_facts
+#cgo windows LDFLAGS: -L${SRCDIR}/../../drivers -lanime_facts
+#cgo CFLAGS: -I${SRCDIR}/../../../anime-facts-core/include
+
+#include "anime_facts_api.h"
+#include <stdlib.h>
+*/
+import "C"
+
 import "github.com/afuradanime/backend/cmd/api/app"
 
 func main() {
+
+	// Set database path
+	C.set_database_path(C.CString("./../anime.db"))
+
 	app.New()
 }
